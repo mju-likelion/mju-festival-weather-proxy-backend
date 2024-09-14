9d942bdc2d3e5ca948e539c6bb604e856f0df8f1
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-14 23:11:37 +0900

    setting: init spring boot

---
+++ src/test/java/org/mju_likelion/weather_proxy/WeatherProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mju_likelion.weather_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
