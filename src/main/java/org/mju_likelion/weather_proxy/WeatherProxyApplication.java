ce961e5cc09b561e8da7b58f5c28c507ca7bb544
Kim Daehyeon <daehyeon3351@gmail.com>
2024-09-22 16:48:10 +0900

    feat: 스케줄링을 허용하도록 변경 - #1

--- src/main/java/org/mju_likelion/weather_proxy/WeatherProxyApplication.java
+++ src/main/java/org/mju_likelion/weather_proxy/WeatherProxyApplication.java
@@ -2,8 +2,10 @@ package org.mju_likelion.weather_proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WeatherProxyApplication {
 
   public static void main(String[] args) {
